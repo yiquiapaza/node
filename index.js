21b149e50b8a14c2d348d656af0ae1def8b8ac3e
Yhonatan Jesus <yiquiapaza@gmail.com>
2022-08-20 00:39:07 -0300

    feat: add nodemon and use basic router

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const express = require('express');
 
+const app = express();
+
+app.use('/',(req, res, next) => {
+    console.log("In the root middleware");
+    res.send('<h1>Root</h1>')
+});
+
+app.use('/add-product',(req, res, next) => {
+    console.log('In the add product middleware');
+    res.send('<h1>The "Add Product" page</h1>');
+});
+
+app.listen(3000);
