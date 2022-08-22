f2d86c966711576d5d4c272b6a27d4532ae9e277
Yhonatan Jesus <yiquiapaza@gmail.com>
2022-08-22 14:54:10 -0300

    feat: add static files and update project

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,12 @@
 const path = require('path');
+const rootDir = require('../util/path');
 const express = require('express');
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
   console.log("ROUTER add-product");
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post("/add-product", (req, res, next) => {
